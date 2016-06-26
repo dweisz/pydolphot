317ce8a1e5d3b553c97050ca6341c29728445c9c
Dan Weisz <dweisz@uw.edu>
2016-06-26 14:56:24 -0700

    fake star script ACS and flc files only

--- make_fakerun.py
+++ make_fakerun.py
@@ -7,6 +7,8 @@ import subprocess
 def makephotfiles(base, nstart, nruns, nimages):
 	for i in range(nstart,nstart+nruns):
 		for j in range(1,nimages):
+			i = np.int(i)
+			j = np.int(j)
 			subprocess.call("ln -s "+base+"."+np.str(j)+".res.fits", base+"_"+np.str(i)+"."+np.str(j)+".res.fits", shell=True)
 			subprocess.call("ln -s "+base+"."+np.str(j)+".psf.fits", base+"_"+np.str(i)+"."+np.str(j)+".psf.fits", shell=True)
 			subprocess.call("ln -s "+base+".info", base+"_"+np.str(i)+".info", shell=True)
